555e8dd6abb36c649b62d11e96a4542dd0a6c584
Art Eschenlauer <eschen@alumni.princeton.edu>
2016-09-13 07:05:09 -0500

    supports numeric, complex, factor, logical, or character data; near.rows now has the semantics of near; near now returns a c of boolean values so that the results of multiple near operations may be combined

--- near.R
+++ near.R
@@ -1,7 +1,7 @@
 near <- function(
   dataframe, # may be specified as identifier or by name
   criterion, # may be specified as identifier or by name
-  expected   # must be specified as identifier or literal; is.numeric(expected) must be true
+  expected   # must be specified as identifier or literal; is expected to be numeric, character, logical, or factor
 ){
   # Function 'near' returns rows from a data.frame that have values in the criterion column within a given tolerance
   #   For example, if the 'mzdel' option is set to 0.00005 with the statement 'options(mzdel=0.00005)',
@@ -10,20 +10,50 @@ near <- function(
 
   # allow dataframe to be specified as identifier or by name
   d_f <- if ("character" != typeof(substitute(dataframe))) dataframe else get(dataframe) 
+
+  # allow criterion to be specified as identifier or by name
+  cr <- if ( "symbol" == typeof(substitute(criterion)) ) paste0(substitute(criterion)) else criterion
+  # handle character, factor, and logical types
+  if ( is.character(expected) || is.factor(expected) ) {
+    return ( base::grepl(pattern = expected, x = as.character(d_f[,cr])) )
+  } else if ( is.logical(expected) ) {
+    return ( d_f[,cr] == expected )
+  }
+
+  # last option is numeric; I don't have experienc
   # return no rows if expected is not numeric
-  if (!is.numeric(expected)) {
-    warning("near: argument 'expected' is not numeric")
+  if (!is.numeric(expected) && !is.complex(expected)) {
+    warning("near: argument 'expected' is not numeric, complex, factor, logical, or character")
     return ( d_f[FALSE,] )
   }
-  # allow criterion to be specified as identifier or by name
-  cr <- paste0(substitute(criterion))
+
   # look up delta among options;
   #   note that to set an option 'foodel', the syntax is 'options(foodel = 1)'
   del_name <- paste0(cr,"del")
   del <- getOption(del_name)
   del <- if (is.null(del)) 0 else del
+
   # return a data.frame with rows where criterion
-  return ( d_f[ abs(d_f[,cr] - expected) < del,] )
+  return ( abs( ( d_f[,cr] ) - expected ) <= del )
+}
+
+near.rows <- function(
+  dataframe, # may be specified as identifier or by name
+  criterion, # may be specified as identifier or by name
+  expected   # must be specified as identifier or literal; is expected to be numeric, character, logical, or factor
+){
+  # allow dataframe to be specified as identifier or by name
+  d_f <- if (is.character(dataframe)) get(dataframe) else dataframe
+  # allow criterion to be specified as identifier or by name
+  cr <- if ( "symbol" == typeof(substitute(criterion)) ) as.character(substitute(criterion),"") else criterion
+  if ( "data.frame" != class(d_f) )
+  {
+    warning("near.rows: dataframe is not data.frame and is not the nane of a data.frame")
+    return ( data.frame() )
+  }
+  return_row <- near(dataframe = d_f, criterion = (cr), expected = expected)
+
+  return ( d_f[ return_row, ] )
 }
 
 # > options(digits=9)
